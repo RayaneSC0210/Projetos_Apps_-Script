ef9751a0546b23fc3e32600494ea1d03cd85ee5f
RayaneSC <rayane.kayroslink@gmail.com>
2025-12-12 19:46:09 -0300

    Delete Botão.js

--- Botão.js
+++
@@ -1,6 +0,0 @@
-function onOpen() {
-  const ui = SpreadsheetApp.getUi();
-  ui.createMenu('Gerar Contrato') // Nome do menu ao lado de "Ajuda"
-    .addItem('Gerar Contrato', 'gerarContratos') // Nome da opção e função vinculada
-    .addToUi();
-}
